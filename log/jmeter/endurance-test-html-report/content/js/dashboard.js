011df32d1cb94fd8be71067ede2e879585d6d6a7
Ifegwu <agbanyimdaniel29@gmail.com>
2022-09-24 17:01:29 +0200

    generating jmeter reports

--- log/jmeter/endurance-test-html-report/content/js/dashboard.js
+++ log/jmeter/endurance-test-html-report/content/js/dashboard.js
@@ -187,7 +187,7 @@ $(document).ready(function() {
     }, [[0, 0]], 3);
 
     // Create statistics table
-    createTable($("#statisticsTable"), {"supportsControllersDiscrimination": true, "overall": {"data": ["Total", 4, 0, 0.0, 956.5, 779, 1134, 956.5, 1134.0, 1134.0, 1134.0, 3.527336860670194, 10.709463183421517, 0.6579309964726632], "isController": false}, "titles": ["Label", "#Samples", "FAIL", "Error %", "Average", "Min", "Max", "Median", "90th pct", "95th pct", "99th pct", "Transactions/s", "Received", "Sent"], "items": [{"data": ["GET All Activities", 4, 0, 0.0, 956.5, 779, 1134, 956.5, 1134.0, 1134.0, 1134.0, 3.527336860670194, 10.709463183421517, 0.6579309964726632], "isController": false}]}, function(index, item){
+    createTable($("#statisticsTable"), {"supportsControllersDiscrimination": true, "overall": {"data": ["Total", 4, 0, 0.0, 725.5, 544, 907, 725.5, 907.0, 907.0, 907.0, 4.410143329658214, 13.389780871003307, 0.8225950937155457], "isController": false}, "titles": ["Label", "#Samples", "FAIL", "Error %", "Average", "Min", "Max", "Median", "90th pct", "95th pct", "99th pct", "Transactions/s", "Received", "Sent"], "items": [{"data": ["GET All Activities", 4, 0, 0.0, 725.5, 544, 907, 725.5, 907.0, 907.0, 907.0, 4.410143329658214, 13.389780871003307, 0.8225950937155457], "isController": false}]}, function(index, item){
         switch(index){
             // Errors pct
             case 3:
--- log/jmeter/stress-test-html-report/content/js/dashboard.js
+++ log/jmeter/stress-test-html-report/content/js/dashboard.js
@@ -173,7 +173,7 @@ $(document).ready(function() {
     });
 
     // Creates APDEX table
-    createTable($("#apdexTable"), {"supportsControllersDiscrimination": true, "overall": {"data": [0.0, 500, 1500, "Total"], "isController": false}, "titles": ["Apdex", "T (Toleration threshold)", "F (Frustration threshold)", "Label"], "items": [{"data": [0.0, 500, 1500, "GET All Activities"], "isController": false}]}, function(index, item){
+    createTable($("#apdexTable"), {"supportsControllersDiscrimination": true, "overall": {"data": [0.75, 500, 1500, "Total"], "isController": false}, "titles": ["Apdex", "T (Toleration threshold)", "F (Frustration threshold)", "Label"], "items": [{"data": [0.75, 500, 1500, "GET All Activities"], "isController": false}]}, function(index, item){
         switch(index){
             case 0:
                 item = item.toFixed(3);
@@ -187,7 +187,7 @@ $(document).ready(function() {
     }, [[0, 0]], 3);
 
     // Create statistics table
-    createTable($("#statisticsTable"), {"supportsControllersDiscrimination": true, "overall": {"data": ["Total", 4, 0, 0.0, 8197.0, 8014, 8380, 8197.0, 8380.0, 8380.0, 8380.0, 0.47732696897374705, 1.4492280727923625, 0.07691303699284009], "isController": false}, "titles": ["Label", "#Samples", "FAIL", "Error %", "Average", "Min", "Max", "Median", "90th pct", "95th pct", "99th pct", "Transactions/s", "Received", "Sent"], "items": [{"data": ["GET All Activities", 4, 0, 0.0, 8197.0, 8014, 8380, 8197.0, 8380.0, 8380.0, 8380.0, 0.47732696897374705, 1.4492280727923625, 0.07691303699284009], "isController": false}]}, function(index, item){
+    createTable($("#statisticsTable"), {"supportsControllersDiscrimination": true, "overall": {"data": ["Total", 4, 0, 0.0, 677.0, 489, 865, 677.0, 865.0, 865.0, 865.0, 4.618937644341801, 13.95604792147806, 0.7442624133949192], "isController": false}, "titles": ["Label", "#Samples", "FAIL", "Error %", "Average", "Min", "Max", "Median", "90th pct", "95th pct", "99th pct", "Transactions/s", "Received", "Sent"], "items": [{"data": ["GET All Activities", 4, 0, 0.0, 677.0, 489, 865, 677.0, 865.0, 865.0, 865.0, 4.618937644341801, 13.95604792147806, 0.7442624133949192], "isController": false}]}, function(index, item){
         switch(index){
             // Errors pct
             case 3:
