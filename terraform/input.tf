cf33bc7d363d1725fd055699dc1ceb2847dcae47
Ifegwu <agbanyimdaniel29@gmail.com>
2022-09-29 16:01:36 +0200

    adding defaults to variables

--- terraform/input.tf
+++ terraform/input.tf
@@ -1,8 +1,14 @@
 # Azure GUIDS
 variable "subscription_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
+variable "client_id" {
+    default = "6672ba44-rf61-4865-a91w-01e3c04e41aa"
+}
+variable "client_secret" {
+    default = "6672ba44-rf61-4865-a91w-01e3c04e41aa"
+}
+variable "tenant_id" {
+    default = "6672ba44-rf61-4865-a91w-01e3c04e41aa"
+}
 
 # Resource Group/Location
 variable "location" {
