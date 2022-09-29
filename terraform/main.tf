af64f088205dea0852f094a2f3e8844171d06cfe
Ifegwu <agbanyimdaniel29@gmail.com>
2022-09-29 12:33:28 +0200

    adding address_prefix_test variable

--- terraform/main.tf
+++ terraform/main.tf
@@ -26,51 +26,51 @@ data "azurerm_resource_group" "test" {
 }
 module "network" {
   source               = "./modules/network"
-  address_space        = var.address_space
-  location             = var.location
-  virtual_network_name = var.virtual_network_name
-  application_type     = var.application_type
+  address_space        = ${var.address_space}"
+  location             = "${var.location}"
+  virtual_network_name = "${var.virtual_network_name}"
+  application_type     = "${var.application_type}"
   resource_type        = "NET"
   resource_group       = data.azurerm_resource_group.test.name
-  address_prefix_test  = var.address_prefix_test
+  address_prefix_test  = "${var.address_prefix_test}"
   demo                 = var.demo
 }
 
 module "nsg-test" {
   source              = "./modules/networksecuritygroup"
-  location            = var.location
-  application_type    = var.application_type
+  location            = "${var.location}"
+  application_type    = "${var.application_type}"
   resource_type       = "NSG"
   resource_group      = data.azurerm_resource_group.test.name
   subnet_id           = module.network.subnet_id_test
-  address_prefix_test = var.address_prefix_test
+  address_prefix_test = "${var.address_prefix_test}"
   demo                = var.demo
 }
 module "appservice" {
   source           = "./modules/appservice"
-  location         = var.location
-  application_type = var.application_type
+  location         = "${var.location"}
+  application_type = "${var.application_type}"
   resource_type    = "AppService"
   resource_group   = data.azurerm_resource_group.test.name
-  demo             = var.demo
+  demo             = "${var.demo"}
 }
 module "publicip" {
   source           = "./modules/publicip"
-  location         = var.location
-  application_type = var.application_type
+  location         = "${var.location}"
+  application_type = "${var.application_type}"
   resource_type    = "publicip"
   resource_group   = data.azurerm_resource_group.test.name
-  demo             = var.demo
+  demo             = "${var.demo}"
 }
 
 module "vm" {
   source                    = "./modules/vm" 
-  location                  = var.location
-  application_type          = var.application_type
+  location                  = "${var.location}"
+  application_type          = "${var.application_type}"
   resource_group            = data.azurerm_resource_group.test.name
   resource_type             = "VM"
   subnet_id                 = module.network.subnet_id_test
-  vm_admin_user             = var.vm_admin_user
+  vm_admin_user             = "${var.vm_admin_user}"
   public_ip_address_id      = module.publicip.public_ip_address_id
-  demo                      = var.demo
+  demo                      = "${var.demo}"
 }
--- terraform/modules/vm/vm.tf
+++ terraform/modules/vm/vm.tf
@@ -22,8 +22,8 @@ resource "azurerm_linux_virtual_machine" "test" {
   resource_group_name   = "${var.resource_group}"
   size                  = "Standard_B1s"
   admin_username        = "${var.vm_admin_user}"
-  # network_interface_ids = [zurerm_network_interface.test.id]
-  network_interface_ids = [element(azurerm_network_interface.test.*.id, count.index)]
+   network_interface_ids = [zurerm_network_interface.test.id,]
+  #network_interface_ids = [element(azurerm_network_interface.test.*.id, count.index)]
   admin_ssh_key {
     #username   = "${var.vm_admin_user}"
     username    = "akwari"
