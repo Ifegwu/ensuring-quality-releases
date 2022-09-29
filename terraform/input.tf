69395dfdd928d49bb22177b7e73edad7140ba28d
Ifegwu <agbanyimdaniel29@gmail.com>
2022-09-29 15:44:20 +0200

    adding default to address_space variable

--- terraform/input.tf
+++ terraform/input.tf
@@ -14,7 +14,9 @@ variable "virtual_network_name" {}
 variable "address_prefix_test" {
     default = "10.5.1.0/24" 
 }
-variable "address_space" {}
+variable "address_space" {
+    default = ["10.5.0.0/16"]
+}
 
 # VM
 variable "vm_admin_user" {
