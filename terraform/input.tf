e03e78c18160259c30b09b67a43b18dd9587cd57
Ifegwu <agbanyimdaniel29@gmail.com>
2022-09-29 16:24:08 +0200

    adding defaults to variables

--- terraform/input.tf
+++ terraform/input.tf
@@ -1,44 +1,44 @@
 # Azure GUIDS
 variable "subscription_id" {
-    default = ""
+    default = "3ca2aacf-c15c-4375-a828-0d64713c2e00"
 }
 variable "client_id" {
-    default = ""
+    default = "7672ba44-ef61-4865-a91c-01e3c04741aa"
 }
 variable "client_secret" {
-    default = ""
+    default = "yD78Q~KSQO~xCTNBIAoZ37YKyLpQla4umQjQqaig"
 }
 variable "tenant_id" {
-    default = ""
+    default = "33e55062-7183-4ac7-b62b-3a2f4f351b3e"
 }
 
 # Resource Group/Location
 variable "location" {
-    default = ""
+    default = "East US"
 }
 variable "resource_group" {
-    default = ""
+    default = "tstate"
 }
 variable "application_type" {
-    default = ""
+    default = "project3App"
 }
 
 # Network
 variable "virtual_network_name" {
-    default = ""
+    default = "project3App-vnet"
 }
 variable "address_prefix_test" {
-    default = "" 
+    default = "10.5.1.0/24" 
 }
 variable "address_space" {
-    default = [""]
+    default = ["10.5.0.0/16"]
 }
 
 # VM
 variable "vm_admin_user" {
-    default = ""
+    default = "azureadmin"
 }
 
 variable "demo" {
-    default = ""
+    default = "devops"
 }
