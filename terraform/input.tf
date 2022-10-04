e9c9dbec0c41aa13a9627bf8b676f349e5415103
Ifegwu <agbanyimdaniel29@gmail.com>
2022-10-05 00:01:18 +0200

    trimming

--- terraform/input.tf
+++ terraform/input.tf
@@ -13,32 +13,16 @@ variable "tenant_id" {
 }
 
 # Resource Group/Location
-variable "location" {
-    default = "East US"
-}
-variable "resource_group" {
-    default = "Azuredevops"
-}
-variable "application_type" {
-    default = "project3App"
-}
+variable "location" {}
+variable "resource_group" {}
+variable "application_type" {}
 
 # Network
-variable "virtual_network_name" {
-    default = "project3App-vnet"
-}
-variable "address_prefix_test" {
-    default = "10.5.1.0/24" 
-}
-variable "address_space" {
-    default = ["10.5.0.0/16"]
-}
+variable "virtual_network_name" {}
+variable "address_prefix_test" {}
+variable "address_space" {}
 
 # VM
-variable "vm_admin_user" {
-    default = "akwari"
-}
+variable "vm_admin_user" {}
 
-variable "demo" {
-    default = "devops"
-}
+variable "demo" {}
